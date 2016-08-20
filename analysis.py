98e3368cabb18304282b61b40cf5ade1740efd3d
corgi <uniuni0123@gmail.com>
2016-08-20 09:50:30 +0900

    add the feature to plot a dfference from MA

--- analysis.py
+++ analysis.py
@@ -1,10 +1,10 @@
-#!/usr/bin/python
+#!/usr/bin/python3.5
 # coding = utf-8
 
 import pandas as pd
 
 class Analysis:
-    def __init__(self, h_prices):
+    def __init__(self, h_prices, ndays):
         d_data = {'date':[], 'open':[], 'high':[], 'low':[], 'close':[], 'volume':[]}
         for l in h_prices:
             d_data['date'].append(l.date)
@@ -14,21 +14,24 @@ class Analysis:
             d_data['close'].append(l.close)
             d_data['volume'].append(l.volume)
         self.df_prices = pd.DataFrame(d_data)
-#        print(self.df_prices)
+        self.ndays = ndays
 
-    def calc_MA(self, ndays=25):
-#        print(self.df_prices['close'].rolling(center=False, window=ndays).mean())
-        return self.df_prices.ix[:,'close'].rolling(center=False, window=ndays).mean()
+    def calc_MA(self):
+        return self.df_prices.ix[:,'close'].rolling(center=False, window=self.ndays).mean().dropna().reset_index(drop = True) #Nanを除外して0から再index
 
     def calc_EM(self):
         pass
 
-    def calc_Diff_from_MA(self, ndays=25):
-        latest_ma = self.calc_MA(ndays).ix[ndays-1]
+    def calc_Diff_from_MA(self):
+        latest_ma = self.calc_MA().ix[0]
         latest_price = self.df_prices.ix[0, 'close']
         diff_rate = (latest_price - latest_ma) / latest_ma
         return latest_price, latest_ma, diff_rate
 
+    def Dist_of_Diff_form_MA(self):
+        ma_list = self.calc_MA()
+        diff_rate_list = (self.df_prices.ix[:, 'close'] - ma_list[:]) /  ma_list[:]
+        return diff_rate_list
 
 if __name__ == '__main__':
     pass
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3.5
 # coding = utf-8
 
 
@@ -7,20 +7,40 @@ import jsm
 import datetime as dt
 from dateutil.relativedelta import relativedelta
 import pandas
+import matplotlib.pyplot as plt
 
 ### import my library ###
 from analysis import Analysis
 
+
 def get_Histrical_Prices(code, start, end):
     return jsm.Quotes().get_historical_prices(code, jsm.DAILY, start_date = start, end_date = end)
 
+
+def generate_MA_Graph(diff_list, ma_days):
+    for i, d in enumerate(ma_days):
+        plt.plot(diff_list[i], linewidth=2.5, linestyle='-', label='{0}MA'.format(d))
+    plt.legend(loc='upper left')
+    plt.show()
+
 if __name__ == '__main__':
     END_DATE = dt.date.today()
-    START_DATE = END_DATE - relativedelta(months=5)
-    CODE = 1417
+    START_DATE = END_DATE - relativedelta(months=24)
+    CODE = 8591
 
+#    today_price = jsm.Quotes().get_price(CODE)
+#    print(today_price)
     hist_prices = get_Histrical_Prices(code = CODE, start=START_DATE, end=END_DATE)
-    ana = Analysis(h_prices=hist_prices)
-    price, ma, diff = ana.calc_Diff_from_MA(ndays=25)
-    print('price: ' + str(price) + ' ma: ' + str(ma) + ' diff: ' + str(diff))
+
+
+    ma_days  = (25, 50, 75)
+    diff_list = []
+    for i in ma_days:
+        ana = Analysis(h_prices=hist_prices, ndays=i)
+        diff_list.append(ana.Dist_of_Diff_form_MA())
+
+    generate_MA_Graph(diff_list, ma_days)
+
+#    price, ma, diff = ana.calc_Diff_from_MA()
+#    print('25days:  price: ' + str(price) + ' ma: ' + str(ma) + ' diff: ' + str(diff))
 
