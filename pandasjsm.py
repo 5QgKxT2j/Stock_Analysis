a7b40e9a7ad72abccd84488fc7134bac6d03b3a1
togawara <regulus.t@gmail.com>
2017-04-05 22:10:16 +0900

    aaa

---
+++ pandasjsm.py
@@ -0,0 +1,18 @@
+# jsm取得データをDataFrame形式で呼び出す
+# 取得データはキャッシュとしてデータベース(sqlite3)へ格納する
+
+import sqlite3
+import datetime as dt
+import pandas as pd
+import pandas.io.sql as psql
+import sys
+
+class pandasjsm():
+    def __init__(self):
+        self.name = "kkk"
+
+    def get_historical_data(self):
+        return
+
+if __name__ == '__main__':
+    pass
