0b3703d0dfd06563e5f751baae6a1ea7e3a55431
togawara <regulus.t@gmail.com>
2017-04-06 09:40:54 +0900

    wrapping basic func(jsm)

--- pandasjsm.py
+++ pandasjsm.py
@@ -6,13 +6,35 @@ import datetime as dt
 import pandas as pd
 import pandas.io.sql as psql
 import sys
+import jsm
 
 class pandasjsm():
     def __init__(self):
         self.name = "kkk"
 
-    def get_historical_data(self):
+    def get_price(self, code):
         return
 
+    def get_historical_prices(self, code, freq, start_date, end_date, all=False):
+        print(code)
+        return
+
+    def __convert_freq(freq):
+        if freq == 'DAILY':
+            return jsm.DAILY
+        elif freq == 'WEEKLY':
+            return jsm.WEEKLY
+        elif freq == 'MONTHLY':
+            return jsm.WEEKLY
+        else:
+            raise 'IllegalExpression'
+
+    def get_inance(self, cdode):
+        return
+
+
 if __name__ == '__main__':
-    pass
+    pj = pandasjsm()
+    pj.get_historical_prices('9433')
+
+    
