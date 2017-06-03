e3c2e83a5356b9cb4a25594af62a7f4f1425331c
togawara <regulus.t@gmail.com>
2017-06-04 01:05:50 +0900

    a

--- main.py
+++ main.py
@@ -25,22 +25,17 @@ if __name__ == '__main__':
 
     pj = pandasjsm()
     df = pj.get_historical_prices(args.target, all=True)
-    df_index = pd.date_range(start = df.index[0], end = df.index[len(df.index)-1])
-    #print(df_index)
+    
+    analyzer.ma_deviation(df)
+    analyzer.ma_deviation(df, window=50)
+    analyzer.ma_deviation(df, window=250)
+    analyzer.momentum(df)
 
-    result = analyzer.ma_deviation(df)
-    result = analyzer.ma_deviation(df, window=50)
-    result = analyzer.ma_deviation(df, window=250)
-    result = analyzer.momentum(df)
-
-    #start = result.index[0]
-    #end = result.index[len(result.index) - 1]
-    #print('start = {start}, end = {end}'.format(start=start, end=end))
     fig, [ax1, ax2, ax3] = plt.subplots(3,1,figsize=(10,15))
-    result.plot(ax = ax1,y=['adj_close', '25MA', '50MA', '250MA'])
+    df.plot(ax = ax1,y=['adj_close', '25MA', '50MA', '250MA'])
     #ax2 = ax.twinx()
-    result.plot(ax = ax2, y=['momentum'])
-    result.plot(ax = ax3, y=['25MA_deviation', '50MA_deviation', '250MA_deviation'])
+    df.plot(ax = ax2, y=['momentum'])
+    df.plot(ax = ax3, y=['25MA_deviation', '50MA_deviation', '250MA_deviation'])
     plt.savefig('./figure/ma_{ccode}.png'.format(ccode=args.target))
     pass
 
