f486eeba2cccf73e6c76f536ce6d43fa6a7f890e
togawara <togawara@users.noreply.github.com>
2017-06-16 21:04:50 +0900

    django: http://qiita.com/kaki_k/items/511611cadac1d0c69c54

--- djangotest/settings.py
+++ djangotest/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'bootstrapform',  # django-bootstrap-form
+    'bootstrapform',  # django-bootstrap-form
     'cms',
 ]
 
