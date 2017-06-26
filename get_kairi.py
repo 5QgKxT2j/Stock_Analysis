c47b0c5716bc38be578d8d5f59c5269f287427d0
ogawara <ogawara@cent7.localdomain>
2017-06-26 19:24:47 +0900

    乖離率の高い銘柄を分析

---
+++ get_kairi.py
@@ -0,0 +1,80 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from pandasjsm import pandasjsm
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from analyzer import analyzer
+import datetime
+
+
+
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+options.add_argument('--window-size=1280,1024')
+driver = webdriver.Chrome(chrome_options=options)
+driver.get('https://jp.kabumap.com/servlets/kabumap/Action?SRC=stockRanking/base&ind=spr25&exch=all&d=a')
+print("サイト名：{0}".format(driver.title)) #=> Google
+#driver.save_screenshot('test.png')
+pj = pandasjsm()
+
+
+for tr in driver.find_elements_by_class_name("KM_NOTINT"):
+    try:
+        
+        code = tr.find_element_by_class_name("KM_CODE").text
+        deviation = tr.find_element_by_class_name("select_number").text
+        if float(deviation) > -15.0:
+            break
+        df = pj.get_historical_prices(code, start_date = datetime.date(2016,1,1))
+        #print(df.tail(1))
+        analyzer.ma_deviation(df)
+        analyzer.momentum(df, period=25)
+        df["max"] = df.adj_close.rolling(window=20).max().shift(periods=-20)
+        df["max"] = df["max"] / df["adj_close"]
+        df["min"] = df.adj_close.rolling(window=20).min().shift(periods=-20)
+        df["min"] = df["min"] / df["adj_close"]
+        #print(df)
+        fig, [ax1, ax2] = plt.subplots(2,1,figsize=(6,8))
+        df["momentum25"] = df["momentum25"] / df["adj_close"]
+        df.plot(ax=ax1, kind='scatter', x="momentum25", y="max", c=df.MA25_deviation, cmap="winter")
+        df.plot(ax=ax2, kind='scatter', x="momentum25", y="min", c=df.MA25_deviation, cmap="winter")
+        fig.suptitle("Today: deviation = {0}, momentum = {1}".format(deviation, df.tail(1).momentum25[0]))
+        plt.savefig("./figure/{0}.png".format(code))
+        plt.close()
+        #break
+    except Exception as e:
+        print("NG")
+        print(e)
+
+
+for tr in driver.find_elements_by_class_name("KM_TINT"):
+    try:
+        
+        code = tr.find_element_by_class_name("KM_CODE").text
+        deviation = tr.find_element_by_class_name("select_number").text
+        if float(deviation) > -15.0:
+            break
+        df = pj.get_historical_prices(code, start_date = datetime.date(2016,1,1))
+        #print(df.tail(1))
+        analyzer.ma_deviation(df)
+        analyzer.momentum(df, period=25)
+        df["max"] = df.adj_close.rolling(window=20).max().shift(periods=-20)
+        df["max"] = df["max"] / df["adj_close"]
+        df["min"] = df.adj_close.rolling(window=20).min().shift(periods=-20)
+        df["min"] = df["min"] / df["adj_close"]
+        #print(df)
+        fig, [ax1, ax2] = plt.subplots(2,1,figsize=(6,8))
+        df["momentum25"] = df["momentum25"] / df["adj_close"]
+        df.plot(ax=ax1, kind='scatter', x="momentum25", y="max", c=df.MA25_deviation, cmap="winter")
+        df.plot(ax=ax2, kind='scatter', x="momentum25", y="min", c=df.MA25_deviation, cmap="winter")
+        fig.suptitle("Today: deviation = {0}, momentum = {1}".format(deviation, df.tail(1).momentum25[0]))
+        plt.savefig("./figure/{0}.png".format(code))
+        plt.close()
+        #break
+    except Exception as e:
+        print("NG")
+        print(e)
+
+driver.quit()
